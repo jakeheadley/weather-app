c25a16cf9b3d44df5a0e46ce2032c0e6fe85f6cb
Jake Headley <jheadley@roomkey.com>
2017-03-30 22:40:16 -0600

    Commiting a basic weather app without styling

--- controller.js
+++ controller.js
@@ -1,5 +1,14 @@
-angular.module('weatherApp').controller('mainCtrl', function($scope){
+angular.module('weatherApp').controller('mainCtrl', function($scope, weatherService){
 
-  $scope.test = 'bOOya!! It works!!';
+  //$scope.test = 'bOOya!! It works!!';
+
+  $scope.getWeather = function(city){
+    console.log('ctrl function fired');
+    return weatherService.getWeather(city).then(function(response){
+      console.log('made it back into crtl');
+      $scope.weather = response;
+      $scope.temp = (response.main.temp * (9/5) - 459.67).toFixed(1) + '℉';
+    });
+  }
 
 });
--- service.js
+++ service.js
@@ -1,1 +1,16 @@
+angular.module('weatherApp').service('weatherService', function($http){
 
+  // $http = 'http://ip-api.com/json';
+  var baseUrl2 = 'http://api.openweathermap.org/data/2.5/weather?q=';
+  var apiKey = 'c54910d1ae4d8e9e7e5bbda457d4ba22';
+
+  this.getWeather = function(city){
+    console.log('at service');
+     return $http.get(baseUrl2+city+"&appid="+apiKey).then(function(response){
+       console.log('got responce');
+       console.log(response.data);
+       return response.data;
+     })
+  };
+
+});
